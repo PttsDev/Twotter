c41b8d6df8647097ce37b2c4da43302e3b24b0a3
PttsDev <47633569+PttsDev@users.noreply.github.com>
2023-06-04 16:24:52 +0200

    input resize

--- components/Tweet/Form/Input.vue
+++ components/Tweet/Form/Input.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="flex items-center flex-shrink-0 p-4 pb-0 ">
-    <div class="flex w-12 items-top">
+    <div class="flex w-12 items-top self-start mt-2">
       <img :src="props.user?.profileImage" alt="" class="inline-block w-10 h-10 rounded-full" />
     </div>
 
     <div class="w-full p-2">
-      <textarea
-        class="w-full h-14 text-lg text-gray-900 placeholder:text-gray-400 bg-transparent border-0 dark:text-white focus:ring-0 overflow-y-clip resize-none"
-        placeholder="What's happening?" v-model="tweetText">
-                </textarea>
+      <textarea ref="tweetInput"
+        class="w-full h-14 text-lg text-gray-900 placeholder:text-gray-400 bg-transparent border-0 dark:text-white focus:ring-0 overflow-hidden resize-none"
+        placeholder="What's happening?" @input="handleTextAreaResize()" v-model="tweetText">
+                                                                        </textarea>
     </div>
   </div>
 
@@ -104,5 +104,11 @@ function handleRemoveMedia() {
   inputMedia.value = ''
   selectedFile.value = null
   mediaUploader.value.value = ''
+}
+
+const tweetInput = ref(null)
+function handleTextAreaResize() {
+  let textArea = tweetInput.value
+  textArea.style.height = textArea.scrollHeight + 'px'
 }
 </script>
